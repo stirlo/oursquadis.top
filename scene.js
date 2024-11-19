2dcc40aefc798a66955e385a3ef93deee7ca938d
stirling campbell <3212918+stirlo@users.noreply.github.com>
2024-11-19 17:33:13 +1100

    Update scene.js

--- scene.js
+++ scene.js
@@ -1,7 +1,7 @@
 // ===== SECTION: IMPORTS =====
-import * as THREE from './three.module.js';
-import { OrbitControls } from './OrbitControls.js';
-import { gsap } from './gsap.min.js';
+import * as THREE from 'https://oursquadis.top/three.module.js';
+import { OrbitControls } from 'https://oursquadis.top/OrbitControls.js';
+import { gsap } from 'https://oursquadis.top/gsap.min.js';
 // ===== END SECTION: IMPORTS =====
 
 // ===== SECTION: CONFIGURATIONS =====
